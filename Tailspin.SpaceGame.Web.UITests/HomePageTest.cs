37692c7cadcdf2def7ff20c7f2ab20af593f8dec
Eric Tan <EricT@x-pressfeeders.com>
2020-02-04 17:46:53 +0800

    Run Selenium UI tests

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -85,6 +85,9 @@ namespace UITests
         [TestCase("download-btn", "pretend-modal")]
         // Screen image
         [TestCase("screen-01", "screen-modal")]
+        [TestCase("screen-02", "screen-modal")]
+        [TestCase("screen-03", "screen-modal")]
+        [TestCase("screen-04", "screen-modal")]
         // // Top player on the leaderboard
         [TestCase("profile-1", "profile-modal-1")]
         public void ClickLinkById_ShouldDisplayModalById(string linkId, string modalId)
